4e98fddc5a9c143bb7de5c438a9677aaa06a0bfb
Marcio Valle De Oliveira e Cruz <mvocruz@hotmail.com>
2021-01-07 02:15:49 +0000

    including sold out badge

--- db/seeds.rb
+++ db/seeds.rb
@@ -71,7 +71,7 @@ cat1.products.create!({
   name:  'Russian Spy Shoes',
   description: Faker::Hipster.paragraph(4),
   image: open_asset('apparel5.jpg'),
-  quantity: 8,
+  quantity: 0,
   price: 1_225.00
 })
 
@@ -96,7 +96,7 @@ cat2.products.create!({
   name:  'Hotdog Slicer',
   description: Faker::Hipster.paragraph(4),
   image: open_asset('electronics2.jpg'),
-  quantity: 3,
+  quantity: 0,
   price: 26.00
 })
 
